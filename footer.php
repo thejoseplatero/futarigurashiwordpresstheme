f2921d8a3615a63efbcb801fb701abad6db2e2a5
Jose Platero <contact@joseplatero.com>
2013-07-14 16:13:20 -0400

    updates

---
+++ footer.php
@@ -0,0 +1,16 @@
+  <div class="footer">
+
+        <span>&copy; Designed by @thejoseplatero<span>
+          <div class="flag">
+            
+          </div>
+
+
+  </div>
+
+</div> <!-- wrapper -->
+
+   <?php wp_footer(); ?>  <!-- hook -->
+
+</body>
+</html>
